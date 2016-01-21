b86d1a99b05ba4915515bf8f85f01e42e2349414
Brandy Carney <brandyscarney@gmail.com>
2016-01-21 15:02:26 -0500

    feat: add confirmCommit prompt and increase version number

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "format cjs";
 
 var wrap = require('word-wrap');
+var colors = require('colors/safe');
 
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
@@ -26,7 +27,7 @@ module.exports = {
     // so that we can populate our commit
     // template.
     //
-    // See inquirer.js docs for specifics.
+    // See inquirer.js docs for specifics: https://github.com/SBoudrias/Inquirer.js/
     // You can also opt to use another input
     // collection library if you prefer.
     cz.prompt([
@@ -82,30 +83,58 @@ module.exports = {
         type: 'input',
         name: 'footer',
         message: 'List any breaking changes or issues closed by this change (e.g references #5077, closes #5077, etc.):\n'
+      }, {
+        type: 'list',
+        name: 'confirmCommit',
+        choices: [
+          {
+            name: 'Yes',
+            value: 'yes'
+          }, {
+            name: 'Abort commit',
+            value: 'no'
+          }
+        ],
+        message: function(answers) {
+          var SEP = '--------------------------------------------------------------';
+          console.log('\n' + SEP + '\n' + buildCommit(answers) + '\n' + SEP + '\n');
+          return 'Are you sure you want to proceed with the commit above?';
+        }
       }
     ], function(answers) {
-
-      var maxLineWidth = 100;
-
-      var wrapOptions = {
-        trim: true,
-        newline: '\n',
-        indent:'',
-        width: maxLineWidth
-      };
-
-      // parentheses are only needed when a scope is present
-      var scope = answers.scope.trim();
-      scope = scope ? '(' + answers.scope.trim() + ')' : '';
-
-      // Hard limit this line
-      var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
-
-      // Wrap these lines at 100 characters
-      var body = wrap(answers.body, wrapOptions);
-      var footer = wrap(answers.footer, wrapOptions);
-
-      commit(head + '\n\n' + body + '\n\n' + footer);
+      if (answers.confirmCommit === 'yes') {
+        commit(buildCommit(answers));
+        console.log(colors.green("Commit has been created."));
+      } else {
+        console.log(colors.red("Commit has been canceled."));
+      }
     });
   }
+}
+
+// Build the commit
+function buildCommit(answers) {
+  var maxLineWidth = 100;
+
+  var wrapOptions = {
+    trim: true,
+    newline: '\n',
+    indent:'',
+    width: maxLineWidth
+  };
+
+  // parentheses are only needed when a scope is present
+  var scope = answers.scope.trim();
+  scope = scope ? '(' + answers.scope.trim() + ')' : '';
+
+  // Hard limit this line
+  var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
+
+  // Wrap these lines at 100 characters
+  var body = wrap(answers.body, wrapOptions);
+  var footer = wrap(answers.footer, wrapOptions);
+
+  var result = head + '\n\n' + body + '\n\n' + footer;
+
+  return result;
 }
